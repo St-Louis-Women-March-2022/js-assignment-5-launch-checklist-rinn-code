205f04152e5f39a2976f1d2e92bd54d1b8181e5b
Rinn Mattox <rinnreed@gmail.com>
2022-07-08 19:56:57 -0500

    added more to event listener

--- scriptHelper.js
+++ scriptHelper.js
@@ -2,47 +2,47 @@
 require('isomorphic-fetch');
 
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
-   // Here is the HTML formatting for our mission target div.
-   /*
-                <h2>Mission Destination</h2>
-                <ol>
-                    <li>Name: </li>
-                    <li>Diameter: </li>
-                    <li>Star: ${star}</li>
-                    <li>Distance from Earth: </li>
-                    <li>Number of Moons: </li>
-                </ol>
-                <img src="">
-   */
+    // Here is the HTML formatting for our mission target div.
+    /*
+                 <h2>Mission Destination</h2>
+                 <ol>
+                     <li>Name: </li>
+                     <li>Diameter: </li>
+                     <li>Star: ${star}</li>
+                     <li>Distance from Earth: </li>
+                     <li>Number of Moons: </li>
+                 </ol>
+                 <img src="">
+    */
 }
 
 function validateInput(testInput) {
-    <script>
-    window.addEventListener("load", function() {
-       let form = document.querySelector("form");
-       form.addEventListener("submit", function(event) {
-          let pilotName = document.querySelector("input[name=pilotName]");
-          let copilotName = document.querySelector("input[name=copilotName]");
-          let fuelLevel =
-          let cargoMass = 
-          if (pilotName.value === "" || teamName.value === "" || fuelLevel.value === "" || cargoMass.value === "") {
-             alert("All fields are required!");
-             event.preventDefault();
+  //  <script>
+        window.addEventListener("load", function() {
+            let form = document.querySelector("form");
+        form.addEventListener("submit", function(event) {
+            let pilotName = document.querySelector("input[name=pilotName]");
+            let copilotName = document.querySelector("input[name=copilotName]");
+            let fuelLevel = document.querySelector("input[name=fuelLevel]");
+            let cargoMass = document.querySelector("input[name=cargoMass]");
+            if (pilotName.value === "" || teamName.value === "" || fuelLevel.value === "" || cargoMass.value === "") {
+                alert("All fields are required!");
+                 event.preventDefault();
           }
        });
     });
- </script> 
+  //  </script>
 }
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-   
+
 }
 
 async function myFetch() {
     let planetsReturned;
 
-    planetsReturned = await fetch().then( function(response) {
-        });
+    planetsReturned = await fetch().then(function (response) {
+    });
 
     return planetsReturned;
 }
@@ -53,5 +53,5 @@ function pickPlanet(planets) {
 module.exports.addDestinationInfo = addDestinationInfo;
 module.exports.validateInput = validateInput;
 module.exports.formSubmission = formSubmission;
-module.exports.pickPlanet = pickPlanet; 
+module.exports.pickPlanet = pickPlanet;
 module.exports.myFetch = myFetch;
