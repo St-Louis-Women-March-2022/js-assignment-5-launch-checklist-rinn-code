921aecdb4378b4725706cbffe95805553245bb5f
Rinn Mattox <rinnreed@gmail.com>
2022-07-08 20:06:10 -0500

    moved event listener & contents to formSubmission

--- scriptHelper.js
+++ scriptHelper.js
@@ -17,25 +17,23 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
 }
 
 function validateInput(testInput) {
-  //  <script>
-        window.addEventListener("load", function() {
-            let form = document.querySelector("form");
+
+}
+
+function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
+    window.addEventListener("load", function() {
+        let form = document.querySelector("form");
         form.addEventListener("submit", function(event) {
             let pilotName = document.querySelector("input[name=pilotName]");
             let copilotName = document.querySelector("input[name=copilotName]");
             let fuelLevel = document.querySelector("input[name=fuelLevel]");
             let cargoMass = document.querySelector("input[name=cargoMass]");
-            if (pilotName.value === "" || teamName.value === "" || fuelLevel.value === "" || cargoMass.value === "") {
+            if (pilotName.value === "" || copilotName.value === "" || fuelLevel.value === "" || cargoMass.value === "") {
                 alert("All fields are required!");
                  event.preventDefault();
           }
        });
     });
-  //  </script>
-}
-
-function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-
 }
 
 async function myFetch() {
