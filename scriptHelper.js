4f6b421b24757f76581b5ecae13a9e6d90eceb2f
Rinn Mattox <rinnreed@gmail.com>
2022-07-19 21:48:34 -0500

    added to destination

--- scriptHelper.js
+++ scriptHelper.js
@@ -2,18 +2,17 @@
 require('isomorphic-fetch');
 
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
-    // Here is the HTML formatting for our mission target div.
-    /*
-                 <h2>Mission Destination</h2>
+    let destination = document.getElementById("missionTarget");
+        destination.innerHTML = 
+                `<h2>Mission Destination</h2>
                  <ol>
-                     <li>Name: </li>
-                     <li>Diameter: </li>
+                     <li>Name: ${name}</li>
+                     <li>Diameter: ${diameter}</li>
                      <li>Star: ${star}</li>
-                     <li>Distance from Earth: </li>
-                     <li>Number of Moons: </li>
+                     <li>Distance from Earth: ${distance}</li>
+                     <li>Number of Moons: ${moons}</li>
                  </ol>
-                 <img src="">
-    */
+                 <img src="${imageUrl}">`;
 }
 
 function validateInput(testInput) {
